656e7871bbda62ebe1434d60fed0164eaa855aeb
Gor Voskanyan <voskanyangor26@gmail.com>
2023-02-20 13:51:53 +0400

    list example

--- main.py
+++ main.py
@@ -1,201 +1,213 @@
-score = 0
-score3 = 0
-score6 = 0
-score9 = 0
-score12 = 0
-x = input('1.#Ո՞ր քաղաքն է ամենախիտ բնակեցվածը աշխարհում:.\
-    #\n A: Լոնդոն\n B: Նյու Յորք\n C: Տոկիո\n D: Փարիզ\n Պատասխան: ')
-for i in x:
-    if x == 'C':
-        score += 1000
-        print('Ճիշտ պատասխան: ','դուք ունեք',score)
-    else:
-        print('Սխալ պատասխան')
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('2.#Որտե՞ղ է մեծացել հայտնի արվեստագետ Միքելանջելոն#\
-        \n A: Փարիզ\n B: Հռոմ\n C: Վիենա\n D: Ֆլորենցիա\n Պատասխան: ')
-for i in x:
-    if x == 'D':
-        score += 1000
-        print('Ճիշտ պատասխան: ','դուք ունեք',score)
-    else:
-        print('Սխալ պատասխան')
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('3.#Հետևյալ ֆիլմերից ո՞րում չի նկարահանվել Լեոնարդո Դի Կապրիոն#\
-        \n A: Տիտանիկ\n B: Ուոլ Սթրիթի գայլը\n C: Ֆոկուս\n D: Բռնիր ինձ, թե կարող ես\n Պատասխան: ')
-for i in x:
-    if x == 'C':
-        score += 1000
-        score3 = score
-        print('Ճիշտ պատասխան: ','դուք ունեք',score3,'Անձեռնմխելի գումար')
-    else:
-        print('Սխալ պատասխան')
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('4.#Ո՞ր պետությունով է հոսում Ամազոն գետը#\
-        \n A: Բրազիլիա\n B: Արգենտինա\n C: Կոլումբիա\n D: Պերու\n Պատասխան: ')
-for i in x:
-    if x == 'A':
-        score += 2000
-        print('Ճիշտ պատասխան: ','դուք ունեք',score)
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score3)
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('5.#Ո՞ր սպորտում է անհրաժեշտ հավաքել ամենաքիչ միավորները#\
-        \n A: Ջրային պոլո\n B: Բատմինտոն\n C: Բոուլինգ\n D: Գոլֆ\n Պատասխան: ')
-for i in x:
-    if x == 'D':
-        score += 3000
-        print('Ճիշտ պատասխան: ','դուք ունեք',score)
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score3)
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('6.#Հունական դիցաբանության մեջ ո՞րն էր վերջին զգացմունքը#\
-        \n A: Սերը\n B: Քաոսը\n C: Քաջությունը\n D: Հույսը\n Պատասխան: ')
-for i in x:
-    if x == 'D':
-        score += 8000
-        score6 = score
-        print('Ճիշտ պատասխան: ','դուք ունեք',score6,'Անձեռնմխելի գումար')
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score3)
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('7.#Քանի՞ սև կետ են ունենում նոր ծնված Դալմատին ցեղատեսակի շները#\
-        \n A: Տասը\n B: Ոչ մի\n C: Հինգ\n D: Քսան\n Պատասխան: ')
-for i in x:
-    if x == 'B':
-        score += 16000
-        print('Ճիշտ պատասխան: ','դուք ունեք',score)
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score6)
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('8.#Մարդու ուղեղը մոտավորապես քանի՞ տոկոսով է բաղկացած ջրից:#\
-        \n A: 40%\n B: 30%\n C: 80%\n D: 20%\n Պատասխան: ')
-for i in x:
-    if x == 'C':
-        score += 32000
-        print('Ճիշտ պատասխան: ','դուք ունեք',score)
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score6)
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('9.#Ի՞նչ արագությամբ են շարժվում մարդու նյարդային ազդակները#\
-        \n A: 570 կմ/ժ\n B: 170 կմ/ժ\n C: 470 կմ/ժ\n D: 270 կմ/ժ\n Պատասխան: ')
-for i in x:
-    if x == 'D':
-        score += 61000
-        score9 = score
-        print('Ճիշտ պատասխան: ','դուք ունեք',score9,'Անձեռնմխելի գումար')
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score6)
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('10.#Սեփականության և ժառանգման իրավունք ունեն…#\
-        \n A: Օլիգարխները\n B: Բոլորը\n C: Պատգամավորները\n D: Հերոսները\n Պատասխան: ')
-for i in x:
-    if x == 'B':
-        score += 125000
-        print('Ճիշտ պատասխան: ','դուք ունեք',score)
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score9)
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('11.#Կարո՞ղ է արդյոք դատավորը լինել կուսակցության անդամ:#\
-        \n A: Պարտադիր է\n B: Ոչ\n C: Այո\n D: Ցանկալի է\n Պատասխան: ')
-for i in x:
-    if x == 'B':
-        score += 250000
-        print('Ճիշտ պատասխան: ','դուք ունեք',score)
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score9)
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('12.#Ո՞վ ունի այնպիսի մատնահետք, ինչպիսին քոնն է#\
-        \n A: Մայրդ\n B: Հայրդ\n C: Ոչ ոք\n D: Պապիկդ\n Պատասխան: ')
-for i in x:
-    if x == 'C':
-        score += 500000
-        score12 = score
-        print('Ճիշտ պատասխան: ','դուք ունեք',score12,'Անձեռնմխելի գումար')
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score9)
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('13.#Ի՞նչ է նշանակում «կարատե» բառը#\
-        \n A: Հաղթանակ\n B: Ճկուն հարված\n C: Վարպետ\n D: Դատարկ ձեռք\n Պատասխան: ')
-for i in x:
-    if x == 'D':
-        score += 1000000
-        print('Ճիշտ պատասխան: ','դուք ունեք',score)
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score12)
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('14.#Ո՞ր թվականին սկսվեց Երկրորդ համաշխարհային պատերազմը#\
-        \n A: 1938թ.\n B: 1941թ.\n C: 1939թ. \n D: 1945թ.\n Պատասխան: ')
-for i in x:
-    if x == 'C':
-        score += 1000000
-        score12 = score
-        print('Ճիշտ պատասխան: ','դուք ունեք',score)
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score12)
-        break
-    y = input('Do you want continue ? insert Y/N:    ')
-    if y == 'N':
-        break
-    else:
-        x = input('15.#Ըստ իր խոստովանության ինչի՞ աստվածն էր Օլե Լուկոյեն՝ Անդերսենի համանուն հեքիաթից#\
-        \n A: Մանկության\n B: Երազների\n C: Հեքիաթների\n D: Գիշերվա\n Պատասխան: ')
-for i in x:
-    if x == 'B':
-        score += 2000000
-        score12 = score
-        print('Ճիշտ պատասխան, դուք ունեք',score,'դուք դարձաք միլիոնատեր!!!')
-    else:
-        print('Սխալ պատասխան: ','դուք ունեք',score12)
-print('Խաղի ավարտ, Արսեն Հովհաննիսյան։')
+# score = 0
+# score3 = 0
+# score6 = 0
+# score9 = 0
+# score12 = 0
+# x = input('1.#Ո՞ր քաղաքն է ամենախիտ բնակեցվածը աշխարհում:.\
+#     #\n A: Լոնդոն\n B: Նյու Յորք\n C: Տոկիո\n D: Փարիզ\n Պատասխան: ')
+# for i in x:
+#     if x == 'C':
+#         score += 1000
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score)
+#     else:
+#         print('Սխալ պատասխան')
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('2.#Որտե՞ղ է մեծացել հայտնի արվեստագետ Միքելանջելոն#\
+#         \n A: Փարիզ\n B: Հռոմ\n C: Վիենա\n D: Ֆլորենցիա\n Պատասխան: ')
+# for i in x:
+#     if x == 'D':
+#         score += 1000
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score)
+#     else:
+#         print('Սխալ պատասխան')
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('3.#Հետևյալ ֆիլմերից ո՞րում չի նկարահանվել Լեոնարդո Դի Կապրիոն#\
+#         \n A: Տիտանիկ\n B: Ուոլ Սթրիթի գայլը\n C: Ֆոկուս\n D: Բռնիր ինձ, թե կարող ես\n Պատասխան: ')
+# for i in x:
+#     if x == 'C':
+#         score += 1000
+#         score3 = score
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score3,'Անձեռնմխելի գումար')
+#     else:
+#         print('Սխալ պատասխան')
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('4.#Ո՞ր պետությունով է հոսում Ամազոն գետը#\
+#         \n A: Բրազիլիա\n B: Արգենտինա\n C: Կոլումբիա\n D: Պերու\n Պատասխան: ')
+# for i in x:
+#     if x == 'A':
+#         score += 2000
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score)
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score3)
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('5.#Ո՞ր սպորտում է անհրաժեշտ հավաքել ամենաքիչ միավորները#\
+#         \n A: Ջրային պոլո\n B: Բատմինտոն\n C: Բոուլինգ\n D: Գոլֆ\n Պատասխան: ')
+# for i in x:
+#     if x == 'D':
+#         score += 3000
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score)
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score3)
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('6.#Հունական դիցաբանության մեջ ո՞րն էր վերջին զգացմունքը#\
+#         \n A: Սերը\n B: Քաոսը\n C: Քաջությունը\n D: Հույսը\n Պատասխան: ')
+# for i in x:
+#     if x == 'D':
+#         score += 8000
+#         score6 = score
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score6,'Անձեռնմխելի գումար')
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score3)
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('7.#Քանի՞ սև կետ են ունենում նոր ծնված Դալմատին ցեղատեսակի շները#\
+#         \n A: Տասը\n B: Ոչ մի\n C: Հինգ\n D: Քսան\n Պատասխան: ')
+# for i in x:
+#     if x == 'B':
+#         score += 16000
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score)
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score6)
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('8.#Մարդու ուղեղը մոտավորապես քանի՞ տոկոսով է բաղկացած ջրից:#\
+#         \n A: 40%\n B: 30%\n C: 80%\n D: 20%\n Պատասխան: ')
+# for i in x:
+#     if x == 'C':
+#         score += 32000
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score)
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score6)
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('9.#Ի՞նչ արագությամբ են շարժվում մարդու նյարդային ազդակները#\
+#         \n A: 570 կմ/ժ\n B: 170 կմ/ժ\n C: 470 կմ/ժ\n D: 270 կմ/ժ\n Պատասխան: ')
+# for i in x:
+#     if x == 'D':
+#         score += 61000
+#         score9 = score
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score9,'Անձեռնմխելի գումար')
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score6)
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('10.#Սեփականության և ժառանգման իրավունք ունեն…#\
+#         \n A: Օլիգարխները\n B: Բոլորը\n C: Պատգամավորները\n D: Հերոսները\n Պատասխան: ')
+# for i in x:
+#     if x == 'B':
+#         score += 125000
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score)
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score9)
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('11.#Կարո՞ղ է արդյոք դատավորը լինել կուսակցության անդամ:#\
+#         \n A: Պարտադիր է\n B: Ոչ\n C: Այո\n D: Ցանկալի է\n Պատասխան: ')
+# for i in x:
+#     if x == 'B':
+#         score += 250000
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score)
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score9)
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('12.#Ո՞վ ունի այնպիսի մատնահետք, ինչպիսին քոնն է#\
+#         \n A: Մայրդ\n B: Հայրդ\n C: Ոչ ոք\n D: Պապիկդ\n Պատասխան: ')
+# for i in x:
+#     if x == 'C':
+#         score += 500000
+#         score12 = score
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score12,'Անձեռնմխելի գումար')
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score9)
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('13.#Ի՞նչ է նշանակում «կարատե» բառը#\
+#         \n A: Հաղթանակ\n B: Ճկուն հարված\n C: Վարպետ\n D: Դատարկ ձեռք\n Պատասխան: ')
+# for i in x:
+#     if x == 'D':
+#         score += 1000000
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score)
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score12)
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('14.#Ո՞ր թվականին սկսվեց Երկրորդ համաշխարհային պատերազմը#\
+#         \n A: 1938թ.\n B: 1941թ.\n C: 1939թ. \n D: 1945թ.\n Պատասխան: ')
+# for i in x:
+#     if x == 'C':
+#         score += 1000000
+#         score12 = score
+#         print('Ճիշտ պատասխան: ','դուք ունեք',score)
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score12)
+#         break
+#     y = input('Do you want continue ? insert Y/N:    ')
+#     if y == 'N':
+#         break
+#     else:
+#         x = input('15.#Ըստ իր խոստովանության ինչի՞ աստվածն էր Օլե Լուկոյեն՝ Անդերսենի համանուն հեքիաթից#\
+#         \n A: Մանկության\n B: Երազների\n C: Հեքիաթների\n D: Գիշերվա\n Պատասխան: ')
+# for i in x:
+#     if x == 'B':
+#         score += 2000000
+#         score12 = score
+#         print('Ճիշտ պատասխան, դուք ունեք',score,'դուք դարձաք միլիոնատեր!!!')
+#     else:
+#         print('Սխալ պատասխան: ','դուք ունեք',score12)
+# print('Խաղի ավարտ, Արսեն Հովհաննիսյան։')
+
+
+
+
+
+
+lstoriginal = []
+new = []
+while (x := input('Insert Name:   ')) != '': lstoriginal.append(x), lstoriginal.sort()
+new = [[i, lstoriginal.count(i)] for i in lstoriginal]
+new = [k for j, k in enumerate(new) if k  not in new[:j]]
+print(new)
